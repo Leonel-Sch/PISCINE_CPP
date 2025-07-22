bbb6f6eac9c24313eea72e399a2d6ac0e6f50062
Leonel Scheuplein <lscheupl@f6r10s4.clusters.42paris.fr>
2025-07-22 15:33:05 +0200

    push nini cpp

--- CPP_0/ex00/megaphone.cpp
+++ CPP_0/ex00/megaphone.cpp
@@ -6,29 +6,19 @@
 /*   By: lscheupl <lscheupl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 15:56:06 by lscheupl          #+#    #+#             */
-/*   Updated: 2025/04/04 16:15:37 by lscheupl         ###   ########.fr       */
+/*   Updated: 2025/04/14 16:55:16 by lscheupl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
 
-char *str_upper(char *str)
-{
-    for(int i = 0;str[i] != '\0'; i++)
-    {
-        if (str[i] > 97 && str[i] < 122)
-            str[i] -= 32;
-    }
-    return str;
-}
-
 int main(int argc, char **argv)
 {
     for (int i = 1; argv[i] !=NULL; i++)
     {
-        str_upper(argv[i]);
-        std :: cout << argv[i];
+        for(int j = 0; argv[i][j] != '\0'; j++)
+            std :: cout << (char)(toupper(argv[i][j]));
     }
     if (argc == 1)
         std :: cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std :: endl;
